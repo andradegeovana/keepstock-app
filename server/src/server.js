e0e141adb38e4b1124156bd1f213e07b45a182fb
junior-slv <dirceu.junior@edu.unifil.br>
2023-11-22 19:43:44 -0300

    Separa os arquivos de front e back, remove arquivos inutilizados

--- server/src/server.js
+++ server/src/server.js
@@ -18,11 +18,12 @@ app.get("/", (req, res) => {
 
 //Conexão com o banco de dados
 const PORT = process.env.PORT || 5000;
+
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(PORT, () => {
-            console.log('Server rodando na porta ${PORT}')
+            console.log(`Server rodando na porta ${PORT}`)
         })
     })
     .catch((err) => console.log(err))

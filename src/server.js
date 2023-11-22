fe2298d70b85791c878a68e941efee9420cc7e36
andradegeovana <geovanaandrade@edu.unifil.br>
2023-11-22 16:58:06 -0300

    routes and middlewares

--- src/server.js
+++ src/server.js
@@ -6,9 +6,18 @@ const cords = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Rotas
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
 
 //Conexão com o banco de dados
+const PORT = process.env.PORT || 5000;
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
